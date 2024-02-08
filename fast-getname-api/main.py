5c69dea3dad1a23ee0a1f0b81d2ca53a25d7cfd5
Prideland-Okoi <prideland.okoi@gmail.com>
2024-02-08 10:07:57 +0100

    Update main.py
    Added link to website

--- fast-getname-api/main.py
+++ fast-getname-api/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Author - prideland.okoi@gmail.com
+# API DOC - https://get-name-api.onrender.com/docs
 from fastapi import FastAPI
 from routers.get_fullname import router as get_namerouter
 
