f2db124ed4359d478a6392cc222fda91860982a7
Prideland-Okoi <prideland.okoi@gmail.com>
2024-02-13 12:06:06 +0100

    Update main.py
    updated the routing import

--- fast-getname-api/main.py
+++ fast-getname-api/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 # Author - prideland.okoi@gmail.com
 # API DOC - https://get-name-api.onrender.com/docs
+import uvicorn
 from fastapi import FastAPI
-from routers.get_fullname import router as get_namerouter
+from routers import get_fullname, get_pdf
+
 
 app = FastAPI()
 
-app.include_router(get_namerouter)
+
+app.include_router(get_fullname.router)
+app.include_router(get_pdf.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
